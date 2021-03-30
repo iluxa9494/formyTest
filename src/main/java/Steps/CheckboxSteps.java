5e3a4e09f88654743fb2830e8c0905bad2d56e22
Ilia Murashkin <iluxa9494@gmail.com>
2021-03-30 22:27:03 +0300

    Checkbox page changes

--- src/main/java/Steps/CheckboxSteps.java
+++ src/main/java/Steps/CheckboxSteps.java
@@ -1,14 +1,30 @@
 package Steps;
 
 import Config.Drive;
-import Pages.AutocompletePage;
 import Pages.CheckboxPage;
-import org.junit.Test;
+import io.cucumber.java.en.Then;
 import org.openqa.selenium.support.PageFactory;
 
 public class CheckboxSteps extends Drive {
     CheckboxPage checkboxPage = PageFactory.initElements(driver, CheckboxPage.class);
 
-//    @Test
+    @Then("Check {string} title has displayed correctly")
+    public void correctTitle(String arg1) {
+        checkboxPage.isCorrectTitle(arg1);
+    }
 
+    @Then("Check all checkboxes has not selected")
+    public void checkboxesNotSelected() {
+        checkboxPage.areCheckboxesNotSelected();
+    }
+
+    @Then("Click on checkbox {string}")
+    public void clickOnTitleOfCheckboxes(String arg1) {
+        checkboxPage.areClickOnCheckbox(arg1);
+    }
+
+    @Then("Check all checkboxes has selected")
+    public void checkboxesSelected() {
+        checkboxPage.areCheckboxesSelected();
+    }
 }
