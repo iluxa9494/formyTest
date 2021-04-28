434bbcb5d2e5406d4cb86ce8a6bd5e0c2232c7c6
Ilia Murashkin <iluxa9494@gmail.com>
2021-04-28 14:04:21 +0300

    Scroll Page changes

--- src/main/java/Pages/ScrollPage.java
+++ src/main/java/Pages/ScrollPage.java
@@ -1,13 +1,132 @@
 package Pages;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class ScrollPage {
-
     WebDriver driver;
+    AutocompletePage autocompletePage = PageFactory.initElements(driver, AutocompletePage.class);
+    @FindBy(id = "name")
+    public static WebElement fullNameInput;
+    @FindBy(id = "date")
+    public static WebElement dateInput;
 
     public ScrollPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    public void hasScrollTextParagraphCheck(String arg1, String arg2) {
+        try {
+            WebElement paragraph = driver.findElement(By.xpath("//p[" + arg1.charAt(0) + "]"));
+            System.out.println(paragraph.getText().equals(arg2) ? arg1 + " paragraph has had correct text PASSED" :
+                    arg1 + " paragraph has not had correct text FAILED");
+        } catch (NoSuchElementException e) {
+            System.out.println("Something has gone wrong FAILED");
+        }
+    }
+
+    public void hasScrollTitleTextCheck(String arg1, String arg2) {
+        try {
+            WebElement titleElement = driver.findElement(By.xpath("//label[text()='" + arg1 + "']"));
+            System.out.println(titleElement.getText().equals(arg2) ? arg2 + " title has been correct PASSED" :
+                    arg2 + " title has not been correct FAILED");
+        } catch (NoSuchElementException e) {
+            System.out.println("Something has gone wrong FAILED");
+        }
+    }
+
+    public void hasScrollPlaceholderTextCheck(String arg1, String arg2) {
+        switch (arg1) {
+            case "Full Name":
+                System.out.println(fullNameInput.getAttribute("placeholder").equals(arg2) ? "Placeholder has had correct text PASSED" :
+                        "Placeholder has not had correct text FAILED");
+                break;
+            case "Date":
+                System.out.println(dateInput.getAttribute("placeholder").equals(arg2) ? "Placeholder has had correct text PASSED" :
+                        "Placeholder has not had correct text FAILED");
+                break;
+        }
+    }
+
+    public void hasScrollFieldUnselectedEnabledCheck(String arg1) {
+        switch (arg1) {
+            case "Full Name":
+                System.out.println(!(fullNameInput.isSelected()) && fullNameInput.isEnabled() ? "Field has been unselected, enabled PASSED" :
+                        "Field has not been unselected or enabled FAILED");
+                break;
+            case "Date":
+                System.out.println(!(dateInput.isSelected()) && dateInput.isEnabled() ? "Field has been unselected, enabled PASSED" :
+                        "Field has not been unselected or enabled FAILED");
+                break;
+        }
+    }
+
+    public void hasScrollFieldDataEnterCheck(String arg1, String arg2) throws Exception {
+        switch (arg2) {
+            case "full name":
+                fullNameInput.sendKeys(arg1);
+                System.out.println(autocompletePage.getInputFieldValue(fullNameInput).equals(arg1) ? "Data has entered PASSED" :
+                        "Data has not entered FAILED");
+                break;
+            case "date":
+                dateInput.sendKeys(arg1);
+                System.out.println(autocompletePage.getInputFieldValue(dateInput).equals(arg1) ? "Data has entered PASSED" :
+                        "Data has not entered FAILED");
+                break;
+        }
+    }
+
+    public void hasScrollFieldDeleteCheck(String arg1) {
+        switch (arg1) {
+            case "full name":
+                fullNameInput.sendKeys(Keys.DELETE);
+                break;
+            case "date":
+                dateInput.sendKeys(Keys.DELETE);
+                break;
+        }
+    }
+
+    public void hasScrollPageToCheck(String arg1) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        switch (arg1) {
+            case "500 px down to the page bottom":
+                js.executeScript("window.scrollBy(0,500)");
+                break;
+            case "250 px up to the page middle":
+                js.executeScript("window.scrollBy(0,-250)");
+                break;
+            case "500 px up to the page top":
+                js.executeScript("window.scrollBy(0,-500)");
+                break;
+            case "250 px down to the page middle":
+                js.executeScript("window.scrollBy(0,250)");
+                break;
+        }
+    }
+
+    public void hasScrollPositionCheck(String arg1) {
+        switch (arg1) {
+            case "bottom":
+                positionCheck(500L, arg1);
+                break;
+            case "middle":
+                positionCheck(250L, arg1);
+                break;
+            case "top":
+                positionCheck(0L, arg1);
+                break;
+            case "after refresh bottom": // offset by 3 px up has been accepted as norm (a feature)
+                positionCheck(497L, arg1);
+                break;
+        }
+    }
+
+    public void positionCheck(Long arg1, String arg2) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        System.out.println((long) js.executeScript("return window.pageYOffset;") == arg1 ?
+                "Scroll has been in " + arg2 + " position PASSED" : "Scroll has not been in " + arg2 + " position FAILED");
+    }
 }
--- src/main/java/Steps/ScrollSteps.java
+++ src/main/java/Steps/ScrollSteps.java
@@ -1,14 +1,50 @@
 package Steps;
 
 import Config.Drive;
-import Pages.AutocompletePage;
 import Pages.ScrollPage;
-import org.junit.Test;
+import io.cucumber.java.en.Then;
 import org.openqa.selenium.support.PageFactory;
 
 public class ScrollSteps extends Drive {
     ScrollPage scrollPage = PageFactory.initElements(driver, ScrollPage.class);
 
-//    @Test
+    @Then("Check {string} text paragraph has a {string} text")
+    public void scrollTextParagraphCheck(String arg1, String arg2) {
+        scrollPage.hasScrollTextParagraphCheck(arg1, arg2);
+    }
 
+    @Then("Check {string} title has the {string} text")
+    public void scrollTitleTextCheck(String arg1, String arg2) {
+        scrollPage.hasScrollTitleTextCheck(arg1, arg2);
+    }
+
+    @Then("Check {string} placeholder has a {string} text")
+    public void scrollPlaceholderTextCheck(String arg1, String arg2) {
+        scrollPage.hasScrollPlaceholderTextCheck(arg1, arg2);
+    }
+
+    @Then("Check {string} field has been unselected, enabled")
+    public void scrollFieldUnselectedEnabledCheck(String arg1) {
+        scrollPage.hasScrollFieldUnselectedEnabledCheck(arg1);
+    }
+
+    @Then("Enter {string} in the {string} field and check data has entered")
+    public void scrollFieldDataEnterCheck(String arg1, String arg2) throws Exception {
+        scrollPage.hasScrollFieldDataEnterCheck(arg1, arg2);
+    }
+
+    @Then("Clear the {string} field via delete")
+    public void scrollFieldDeleteCheck(String arg1) {
+        scrollPage.hasScrollFieldDeleteCheck(arg1);
+    }
+
+    @Then("Scroll page on {string}")
+    public void scrollPageToCheck(String arg1) {
+        scrollPage.hasScrollPageToCheck(arg1);
+    }
+
+    @Then("Check scroll element has been {string} position")
+    public void scrollPositionCheck(String arg1) {
+        scrollPage.hasScrollPositionCheck(arg1);
+    }
 }
